a4ccd54c4fd0e44f9a954cacc9899d7da481668d
Karan Thakar <karanthakar2834@gmail.com>
2025-01-11 12:48:46 +0530

    aa

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
 const connectTODb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 
-const app = express();
 connectTODb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
